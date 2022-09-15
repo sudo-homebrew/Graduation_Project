2b1f057f0a439aa633d8443bc3764c1a5db598be
MOLOZISE <wchs0314@gmail.com>
2022-09-15 19:54:23 +0900

    build turtlebot3, turtlebot3 related files

---
+++ turtlebot3_ws/build/turtlebot3_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,5 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/molozise/turtlebot3_ws/install/dynamixel_sdk/share/dynamixel_sdk/package.sh"
+. "/home/molozise/turtlebot3_ws/install/turtlebot3_description/share/turtlebot3_description/package.sh"
+. "/home/molozise/turtlebot3_ws/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
+. "/home/molozise/turtlebot3_ws/install/turtlebot3_node/share/turtlebot3_node/package.sh"
