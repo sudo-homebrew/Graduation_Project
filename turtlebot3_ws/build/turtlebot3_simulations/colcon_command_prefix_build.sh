7a743ef00e06b03ac67762bdc324a46523d28404
MOLOZISE <wchs0314@gmail.com>
2022-10-01 15:14:23 +0900

    build turtlebot3 folders, little problem in teleop folder

---
+++ turtlebot3_ws/build/turtlebot3_simulations/colcon_command_prefix_build.sh
@@ -0,0 +1,5 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/molozise/turtlebot3_ws/install/turtlebot3_description/share/turtlebot3_description/package.sh"
+. "/home/molozise/turtlebot3_ws/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
+. "/home/molozise/turtlebot3_ws/install/turtlebot3_fake_node/share/turtlebot3_fake_node/package.sh"
+. "/home/molozise/turtlebot3_ws/install/turtlebot3_gazebo/share/turtlebot3_gazebo/package.sh"
---
+++ turtlebot3_ws/build/turtlebot3_teleop/share/turtlebot3_teleop/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/molozise/turtlebot3_ws/build/turtlebot3_teleop"
