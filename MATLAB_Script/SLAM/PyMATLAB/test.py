2cfac6c95c2179f06f4a0ffcd9a23277edc1d3c6
sudo-homebrew <70418121+sudo-homebrew@users.noreply.github.com>
2022-10-06 19:10:25 +0900

    Python code error fixed
    Success (Finally....)

--- MATLAB_Script/SLAM/PyMATLAB/test.py
+++ MATLAB_Script/SLAM/PyMATLAB/test.py
@@ -1,14 +1,22 @@
 import matlab.engine
-eng = matlab.engine.start_matlab('-nojvm')
-result = eng.PySLAM(25, 'matlab_example_robot', 10)
+import time
 
-pause(4)
+print("Launching MATLAB")
+eng = matlab.engine.start_matlab()
+print("MATLAB has launched")
+
+# How to use PySLAM
+# PySLAM(ROS_DOMAIN_ID, ROS2DomainID, Duration)
+result = eng.PySLAM(25, 'matlab_example_robot', 10)
+print("Starting PySLAM")
+
+time.sleep(4)
 
 occmap = eng.PyGetMap()
 
 print(occmap)
 
-pause(4)
+time.sleep(4)
 
 print(occmap)
 print(result)
