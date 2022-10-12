5cb68bf5182c3e69d99a58a4ad84f090ea189e50
MOLOZISE <83456681+MOLOZISE@users.noreply.github.com>
2022-10-12 16:57:24 +0900

    Update drl_trainer.py

--- turtlebot3_ws/src/turtlebot3_machine_learning/turtlebot3_dqn/turtlebot3_dqn/drl_trainer/drl_trainer.py
+++ turtlebot3_ws/src/turtlebot3_machine_learning/turtlebot3_dqn/turtlebot3_dqn/drl_trainer/drl_trainer.py
@@ -17,8 +17,10 @@ from stable_baselines3 import PPO
 
 import time
 
+
 import gym
 from gym import spaces
+
 
 # import matlabs engine
 
@@ -45,14 +47,13 @@ class gym_NavEnv(gym.Env):
         # How to use PySLAM
         # PySLAM(ROS_DOMAIN_ID, ROS2DomainID, Duration)
         result = self.matlab_slam.PySLAM(30, 'matlab_example_robot', 10)
-
+        
         print("Starting PySLAM")
         rclpy.init(args=None)
         self.drl_trainer = ros_NavEnv()
-        # rclpy.spin(self.drl_trainer)
-        # self.drl_trainer.destroy()
-        # rclpy.shutdown()
-
+        #rclpy.spin(self.drl_trainer)
+        #self.drl_trainer.destroy()
+        #rclpy.shutdown()
     def step(self, action):
         for _ in range(10):
             rclpy.spin_once(self.drl_trainer)
@@ -76,7 +77,6 @@ class gym_NavEnv(gym.Env):
         time.sleep(0.1)
 
         return observation, reward, done, info
-
     def reset(self):
         time.sleep(1)
         if self.drl_trainer.done:
@@ -93,15 +93,12 @@ class gym_NavEnv(gym.Env):
         occmap = self.matlab_slam.PyGetMap()
         print(occmap)
         return observation  # reward, done, info can't be included
-
     def render(self, mode='human'):
         pass
-
-    def close(self):
+    def close (self):
         self.drl_trainer.destroy()
         rclpy.shutdown()
         pass
-
 
 class ros_NavEnv(Node):
     def __init__(self):
@@ -157,8 +154,8 @@ class ros_NavEnv(Node):
         # Matlab SLAM
         #####################
         self.map_sub = self.create_subscription(
-            OccupancyGrid,  # msg_type same
-            'map',  # map_info willbe
+            OccupancyGrid, #msg_type same
+            'map', # map_info willbe
             self.map_callback,
             qos)
         ####################
@@ -173,13 +170,11 @@ class ros_NavEnv(Node):
     """*******************************************************************************
     ** Callback functions and relevant functions
     *******************************************************************************"""
-
     ######
     def map_callback(self, msg):
         print(msg)
         # 7568 = 86 * 88
         print(len(msg.data))
-
     ######
 
     def goal_pose_callback(self, msg):
@@ -192,12 +187,12 @@ class ros_NavEnv(Node):
         _, _, self.last_pose_theta = self.euler_from_quaternion(msg.pose.pose.orientation)
 
         goal_distance = math.sqrt(
-            (self.goal_pose_x - self.last_pose_x) ** 2
-            + (self.goal_pose_y - self.last_pose_y) ** 2)
+            (self.goal_pose_x-self.last_pose_x)**2
+            + (self.goal_pose_y-self.last_pose_y)**2)
 
         path_theta = math.atan2(
-            self.goal_pose_y - self.last_pose_y,
-            self.goal_pose_x - self.last_pose_x)
+            self.goal_pose_y-self.last_pose_y,
+            self.goal_pose_x-self.last_pose_x)
 
         goal_angle = path_theta - self.last_pose_theta
         if goal_angle > math.pi:
@@ -219,7 +214,7 @@ class ros_NavEnv(Node):
         pre_state = self.scan_ranges[0::45]
         state = []
 
-        # print(state)
+        #print(state)
         state.append(float(self.goal_distance / 3.5))
         state.append(float(self.goal_angle / 3.5))
         for scan in pre_state:
@@ -231,8 +226,8 @@ class ros_NavEnv(Node):
             elif scan < -1:
                 scan = -1
             state.append(scan)
-        # print(state)
-        # print(state)
+        #print(state)
+        #print(state)
         # for data in state:
         #     if not (data >= -1 and data <= 1):
         #         print(state)
@@ -282,7 +277,7 @@ class ros_NavEnv(Node):
         action = request.action
         twist = Twist()
         twist.linear.x = 0.3
-        twist.angular.z = ((self.action_size - 1) / 2 - action) * 1.5
+        twist.angular.z = ((self.action_size - 1)/2 - action) * 1.5
         self.cmd_vel_pub.publish(twist)
 
         response.state = self.get_state()
@@ -296,16 +291,16 @@ class ros_NavEnv(Node):
 
         if request.init is True:
             self.init_goal_distance = math.sqrt(
-                (self.goal_pose_x - self.last_pose_x) ** 2
-                + (self.goal_pose_y - self.last_pose_y) ** 2)
+                (self.goal_pose_x-self.last_pose_x)**2
+                + (self.goal_pose_y-self.last_pose_y)**2)
 
         return response
 
     def get_reward(self, action):
-        yaw_reward = 1 - 2 * math.sqrt(math.fabs(self.goal_angle / math.pi))
+        yaw_reward = 1 - 2*math.sqrt(math.fabs(self.goal_angle / math.pi))
 
         distance_reward = (2 * self.init_goal_distance) / \
-                          (self.init_goal_distance + self.goal_distance) - 1
+            (self.init_goal_distance + self.goal_distance) - 1
 
         # Reward for avoiding obstacles
         if self.min_obstacle_distance < 0.25:
@@ -317,25 +312,24 @@ class ros_NavEnv(Node):
 
         # + for succeed, - for fail
         if self.succeed:
-            # print(self.succeed)
+            #print(self.succeed)
             reward += 5
-            # print(reward)
+            #print(reward)
         elif self.fail:
-            # print(self.fail)
+            #print(self.fail)
             reward += -10
-            # print(reward)
-        # print(self.succeed)
-        # print(self.fail)
-        # print(yaw_reward)
-        # print(distance_reward)
-        # print(obstacle_reward)
+            #print(reward)
+        #print(self.succeed)
+        #print(self.fail)
+        #print(yaw_reward)
+        #print(distance_reward)
+        #print(obstacle_reward)
         print(reward)
         return reward
 
     """*******************************************************************************
     ** Below should be replaced when porting for ROS 2 Python tf_conversions is done.
     *******************************************************************************"""
-
     def euler_from_quaternion(self, quat):
         """
         Converts quaternion (w in last place) to euler roll, pitch, yaw
@@ -346,23 +340,22 @@ class ros_NavEnv(Node):
         z = quat.z
         w = quat.w
 
-        sinr_cosp = 2 * (w * x + y * z)
-        cosr_cosp = 1 - 2 * (x * x + y * y)
+        sinr_cosp = 2 * (w*x + y*z)
+        cosr_cosp = 1 - 2*(x*x + y*y)
         roll = numpy.arctan2(sinr_cosp, cosr_cosp)
 
-        sinp = 2 * (w * y - z * x)
+        sinp = 2 * (w*y - z*x)
         pitch = numpy.arcsin(sinp)
 
-        siny_cosp = 2 * (w * z + x * y)
-        cosy_cosp = 1 - 2 * (y * y + z * z)
+        siny_cosp = 2 * (w*z + x*y)
+        cosy_cosp = 1 - 2 * (y*y + z*z)
         yaw = numpy.arctan2(siny_cosp, cosy_cosp)
 
         return roll, pitch, yaw
 
-
 class Trainer():
     def __init__(self):
-        # super().__init__('trainer')
+        #super().__init__('trainer')
         print("0")
         self.env = gym_NavEnv(n_actions=5)
         print("1")
@@ -374,10 +367,8 @@ class Trainer():
         model.learn(total_timesteps=5000)
         model.save("ppo_")
 
-
 def main(args=None):
     Trainer()
-
 
 if __name__ == '__main__':
     main()
