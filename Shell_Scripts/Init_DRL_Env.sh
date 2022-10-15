593f5386e44ac5b9d26bdc9ce92761a599d27153
sudo-homebrew <70418121+sudo-homebrew@users.noreply.github.com>
2022-10-15 18:25:06 +0900

    Comments fixed
    Some comments are fixed

--- Shell_Scripts/Init_DRL_Env.sh
+++ Shell_Scripts/Init_DRL_Env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This shell script is for run Gazebo World with Dynamic Obstacles2
+#This shell script is for Initialise deep reinforcement learning environment
 
 #ROS2 environment setting
 source /opt/ros/foxy/setup.bash && source ~/turtlebot3_ws/install/local_setup.bash
--- Shell_Scripts/Start_DRL_Training.sh
+++ Shell_Scripts/Start_DRL_Training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This shell script is for run Gazebo World with Dynamic Obstacles
+#This shell script is for starting deep reinforcement learning training
 
 #Run ROS2 drl_trainer
 ros2 run turtlebot3_dqn drl_trainer
