8329838dc3a8d8a82a4b5fd0f6535c0d3fee560c
Sünghjöp Nam <moollyska@gmail.com>
2022-10-19 14:09:28 +0900

    Test Directory created
    This is test directory for architecture changes

---
+++ Test/ReturnSLAM_Alg.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = ReturnSLAM_Alg(inputArg1,inputArg2)
+%RETURNSLAM_ALG Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
