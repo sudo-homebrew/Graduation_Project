ab18c108d42f5808ce4983a55dc96c783d225b52
sudo-homebrew <70418121+sudo-homebrew@users.noreply.github.com>
2022-10-20 14:36:56 +0900

    Update UpdteSLAM.m
    Test code error fixed
    add some error conditions

--- Test/UpdteSLAM.m
+++ Test/UpdteSLAM.m
@@ -2,7 +2,10 @@ function [Neu_slamAlg, isScanAccepted] = UpdteSLAM(slamAlg, scanMsg)
 %UPDTESLAM Summary of this function goes here
 %   Detailed explanation goes here
     if ~isa(scanMsg,'struct')
+        isScanAccepted = 0;
         Neu_slamAlg = slamAlg;
+
+        return
     end
 
 
